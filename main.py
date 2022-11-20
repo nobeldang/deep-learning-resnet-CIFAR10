1393f5c124255df4cb221f5c24b34c48970f27d6
Shivansh Sharma <shivanshsharma8@gmail.com>
2022-11-20 17:58:02 -0500

    Added Plot and History

--- main.py
+++ main.py
@@ -10,16 +10,28 @@ import torchvision.transforms as transforms
 
 import os
 import argparse
+import pickle
 
-import resnet
-from utils import progress_bar
+from resnet import *
+from utils import progress_bar, save_plot_over_training
 
 
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
 parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
-parser.add_argument('--resume', '-r', action='store_true',
-                    help='resume from checkpoint')
+parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint')
+parser.add_argument('--epochs', default=50, type=int, help='num epochs')
+parser.add_argument('--mname', required=True, type=str, help='unique model name')
 args = parser.parse_args()
+
+if not os.path.isdir('results'):
+    os.mkdir('results')
+
+model_name = args.mname
+
+if os.path.isdir('./results/'+model_name):
+    raise ValueError('Model with name : '+model_name+' already exist!\nPlease enter a differenet model name.')
+else:
+    os.mkdir('./results/'+model_name)
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 best_acc = 0  # best test accuracy
@@ -54,9 +66,32 @@ classes = ('plane', 'car', 'bird', 'cat', 'deer',
 
 # Model
 print('==> Building model..')
-
-net = resnet.ResNet18()
+# net = VGG('VGG19')
+net = ResNet18()
+# net = PreActResNet18()
+# net = GoogLeNet()
+# net = DenseNet121()
+# net = ResNeXt29_2x64d()
+# net = MobileNet()
+# net = MobileNetV2()
+# net = DPN92()
+# net = ShuffleNetG2()
+# net = SENet18()
+# net = ShuffleNetV2(1)
+# net = EfficientNetB0()
+# net = RegNetX_200MF()
+# net = SimpleDLA()
 net = net.to(device)
+
+
+## Checking the number of trainable parameters
+pytorch_total_params = sum(p.numel() for p in net.parameters() if p.requires_grad)
+print()
+print("#"*50)
+print("Number of Parameters to train: ", pytorch_total_params)
+print("#"*50)
+print()
+
 if device == 'cuda':
     net = torch.nn.DataParallel(net)
     cudnn.benchmark = True
@@ -99,6 +134,10 @@ def train(epoch):
         progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
                      % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
 
+    avg_train_loss = train_loss/total
+    avg_train_acc = 100.*correct/total
+    return avg_train_loss, avg_train_acc
+
 
 def test(epoch):
     global best_acc
@@ -131,11 +170,38 @@ def test(epoch):
         }
         if not os.path.isdir('checkpoint'):
             os.mkdir('checkpoint')
-        torch.save(state, './checkpoint/ckpt.pth')
+        torch.save(state, './checkpoint/'+model_name+'_ckpt.pth')
         best_acc = acc
+    
+    avg_val_loss = test_loss/total
+    avg_val_acc = 100.*correct/total
+    return avg_val_loss, avg_val_acc
 
 
-for epoch in range(start_epoch, start_epoch+200):
-    train(epoch)
-    test(epoch)
+history = {'train_loss':[], 'val_loss':[], 'train_acc':[], 'val_acc':[]}
+
+for epoch in range(start_epoch, start_epoch+args.epochs):    
+    t_loss, t_acc = train(epoch)
+    v_loss, v_acc = test(epoch)
     scheduler.step()
+
+    history['train_loss'].append(t_loss)
+    history['train_acc'].append(t_acc)
+    history['val_loss'].append(v_loss)
+    history['val_acc'].append(v_acc)
+
+### Saving History
+hist_file = './results/' + model_name + '/history.pkl'
+with open(hist_file, 'wb') as f:
+    pickle.dump(history, f)
+
+### Saving Training Plot
+plot_file = './results/' + model_name + '/training_plot.png'
+plot_title = 'Parameters: '+str(pytorch_total_params)+' | lr :'+str(args.lr)
+save_plot_over_training(history, plot_title, plot_file)
+
+
+print('\nTraining Complete !')
+print('Best Accuracy: ',round(best_acc,4),'%')
+print('Best Model saved in /checkpoint/'+model_name+'_ckpt.pth')
+print('History and Plots saved in /results/'+model_name)
